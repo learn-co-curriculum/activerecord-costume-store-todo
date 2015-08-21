bb437cb7c5bd06d5f0911610fbd5663c792d0bc9
George Toothman <george.toothman@flatironschool.com>
2015-08-20 22:07:09 -0400

    add migrations for tables

--- db/migrations/003_create_haunted_houses.rb
+++ db/migrations/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |t|
+      t.text :name
+      t.text :location
+      t.text :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.text :opening_date
+      t.text :closing_date
+      t.string :long_description
+    end
+  end
+end
